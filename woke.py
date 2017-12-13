1732cca4a607a45b84c38ac086017feea90c2990
Nicholas <32025590+horsieeee@users.noreply.github.com>
2017-12-13 09:05:45 -0500

    Update woke.py

--- woke.py
+++ woke.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class WoketualMachine:
   # The Wokescript VM
   def __init__(self):
@@ -21,3 +24,25 @@ class WoketualMachine:
     
   def load_val(self, num):
     self._append(num)
+
+def lex(characters, token_exprs):
+    pos = 0
+    tokens = []
+    while pos < len(characters):
+        match = None
+        for token_expr in token_exprs:
+            pattern, tag = token_expr
+            regex = re.compile(pattern)
+            match = regex.match(characters, pos)
+            if match:
+                text = match.group(0)
+                if tag:
+                    token = (text, tag)
+                    tokens.append(token)
+                break
+        if not match:
+            sys.stderr.write('Illegal character: %s\\n' % characters[pos])
+            sys.exit(1)
+        else:
+            pos = match.end(0)
+    return tokens
